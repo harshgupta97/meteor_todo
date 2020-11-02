bb8783e30cddcc36c804d0c7921070ddff39bdcb
Harsh Gupta <warmachine476@gmail.com>
2020-11-02 18:01:15 +0530

    submit handler

--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
+import { TasksCollection } from "../api/TasksCollection";
 
 export const TaskForm = ({}) => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) return;
+
+    TasksCollection.insert({
+      text: text.trim(),
+      createdAt: new Date(),
+    });
+
+    setText("");
+  };
   return (
     <div>
-      <input type="text" placeholder="Enter task" />
-      <button type="submit">Add task</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Add task</button>
+      </form>
     </div>
   );
 };
