1de0a088e3b2e8e5ef76f0322433aca38170cc3d
Harsh Gupta <warmachine476@gmail.com>
2020-11-02 17:35:38 +0530

    fetching tasks data from database using useTracker

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Task } from "./Task";
+import { useTracker } from "meteor/react-meteor-data";
+import { TasksCollection } from "../api/TasksCollection";
 
-const tasks = [
-  { _id: 1, text: "First Task" },
-  { _id: 2, text: "Second Task" },
-  { _id: 3, text: "Third Task" },
-];
+// const tasks = [
+//   { _id: 1, text: "First Task" },
+//   { _id: 2, text: "Second Task" },
+//   { _id: 3, text: "Third Task" },
+// ];
 
-export const App = () => (
-  <div>
-    <h2>Simple todo app</h2>
-    <ul>
-      {tasks.map((task) => (
-        <Task key={task._id} task={task} />
-      ))}
-    </ul>
-  </div>
-);
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  return (
+    <div>
+      <h2>Simple todo app</h2>
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
+  );
+};
