38256ce59c9d8b1eda102aa5b6d6b0b1e4a6e30c
Harsh Gupta <warmachine476@gmail.com>
2020-11-02 17:13:48 +0530

    removing links and links server code from server main.js

--- server/main.js
+++ server/main.js
@@ -1,31 +1,22 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { TasksCollection } from '../imports/api/TasksCollection';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
+// Insert
+const insertTask = taskText => TasksCollection.insert({ text: taskText })
+
+// Default task
+const tasks = [
+    'First Task',
+    'Second Task',
+    'Third Task',
+    'Fourth Task',
+    'Fifth Task',
+    'Sixth Task',
+    'Seventh Task'
+]
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
-  }
+    if (TasksCollection.find().count() === 0) {
+        tasks.forEach(insertTask)
+    }
 });
