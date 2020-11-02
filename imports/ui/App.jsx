91e251fbbd95ba3d36cd565552d37b339e32c7a5
Harsh Gupta <warmachine476@gmail.com>
2020-11-02 17:53:17 +0530

    update app component with task form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Task } from "./Task";
 import { useTracker } from "meteor/react-meteor-data";
 import { TasksCollection } from "../api/TasksCollection";
+import { TaskForm } from "./TaskForm";
 
 // const tasks = [
 //   { _id: 1, text: "First Task" },
@@ -14,6 +15,7 @@ export const App = () => {
   return (
     <div>
       <h2>Simple todo app</h2>
+      <TaskForm />
       <ul>
         {tasks.map((task) => (
           <Task key={task._id} task={task} />
